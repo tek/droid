6962338f29480c2e89eaa12abc863f404b7c005e
tek <torstenschmits@gmail.com>
2014-11-30 20:45:52 +0100

    general-purpose ternary operator

--- src/util/core_ext/control.scala
+++ src/util/core_ext/control.scala
@@ -7,4 +7,13 @@ trait Control
       callback
     }
   }
+
+  implicit class `ternary operator condition`(cond: Boolean) {
+    def ?[A](v: A) = if (cond) Some(v) else None
+  }
+
+  implicit class `ternary operator alternative`[A](o: Option[A]) {
+    def /[B >: A](alternative: ⇒ Option[B]) = o orElse alternative
+    def /[B >: A](default: ⇒ B) = o getOrElse default
+  }
 }
