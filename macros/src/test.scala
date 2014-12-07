5337b8f8ff3d69e08e41a10460ccaa8cd7e9b396
tek <torstenschmits@gmail.com>
2014-12-07 09:35:12 +0100

    remove test macro

--- macros/src/test.scala
+++ macros/src/test.scala
@@ -5,10 +5,4 @@ import scala.reflect.macros.blackbox.{ Context ⇒ MacroContext }
 
 object MacrosTest
 {
-  def instFrag[F]: F = macro instFragImpl[F]
-
-  def instFragImpl[F: c.WeakTypeTag](c: MacroContext) = {
-    import c.universe._
-    q"new ${weakTypeOf[F]}"
-  }
 }
