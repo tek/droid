d76ac94a697ef1f72273984dc7a041b9651f729a
tek <torstenschmits@gmail.com>
2014-12-15 14:27:22 +0100

    helpers for stringifying a class name for ClassTag and Class

--- src/util/core_ext/metadata.scala
+++ src/util/core_ext/metadata.scala
@@ -1,8 +1,18 @@
 package tryp.droid.util
 
+import scala.reflect.ClassTag
+
 trait MetadataExt
 {
   implicit class `class name shortcut`(a: AnyRef) {
     def className = a.getClass.getSimpleName
+  }
+
+  implicit class `class name shortcut for ClassTag`(a: ClassTag[_]) {
+    def className = a.runtimeClass.getSimpleName
+  }
+
+  implicit class `class name shortcut for Class`(a: Class[_]) {
+    def className = a.getSimpleName
   }
 }
