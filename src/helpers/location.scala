be812d9f07f40ab79ae3ec522f4eb12728809f19
tek <torstenschmits@gmail.com>
2014-12-22 04:56:34 +0100

    add geofence helpers

--- src/helpers/location.scala
+++ src/helpers/location.scala
@@ -1,15 +1,20 @@
 package tryp.droid
 
+import android.app.PendingIntent
 import android.os.Bundle
 
 import com.google.android.gms.location.{LocationServices,_}
-import LocationServices.{FusedLocationApi ⇒ Api}
-import com.google.android.gms.common.ConnectionResult
-import com.google.android.gms.common.api.GoogleApiClient
+import LocationServices.{FusedLocationApi ⇒ LocApi, GeofencingApi ⇒ GeoApi}
+import com.google.android.gms.common._
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.common.api._
+
+case class GeofenceData(id: String, lat: Double, long: Double)
 
 trait Locations
 extends tryp.droid.Basic
 with tryp.droid.util.CallbackMixin
+with LocationListener
 {
   class LocationCallbacks(owner: Locations)
   extends GoogleApiClient.OnConnectionFailedListener
@@ -20,15 +25,20 @@ with tryp.droid.util.CallbackMixin
     }
 
     def onConnectionFailed(connectionResult: ConnectionResult) {
+      Log.e("Location connection failed")
     }
 
     def onConnectionSuspended(cause: Int) {
+      Log.e("Location connection suspended")
     }
   }
 
+  def playServicesAvailable =
+    GooglePlayServicesUtil.isGooglePlayServicesAvailable(context)
+
   private lazy val locationCallbacks = new LocationCallbacks(this)
 
-  private lazy val apiClient =
+  lazy val apiClient =
     new GoogleApiClient.Builder(context)
     .addApi(LocationServices.API)
     .addConnectionCallbacks(locationCallbacks)
@@ -49,8 +59,38 @@ with tryp.droid.util.CallbackMixin
   def locations = apiClient.isConnected ? apiClient
 
   def lastLocation = {
-    locations map { Api.getLastLocation(_) }
+    locations flatMap { client ⇒ Option(LocApi.getLastLocation(client)) }
     }
+
+  def requestGeofences(
+    intent: PendingIntent, locations: Seq[GeofenceData]
+  ) {
+    val builder = new GeofencingRequest.Builder
+    locations foreach { loc ⇒ builder.addGeofence(geofence(loc)) }
+    GeoApi.addGeofences(apiClient, builder.build, intent)
+  }
+
+  val expirationDuration = 24 * 3600 * 1000
+
+  def geofence(fence: GeofenceData) = {
+    new Geofence.Builder()
+      .setRequestId(fence.id.toString)
+      .setTransitionTypes(
+        Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_DWELL
+        | Geofence.GEOFENCE_TRANSITION_EXIT
+      )
+      .setCircularRegion(fence.lat, fence.long, 50)
+      .setExpirationDuration(expirationDuration)
+      .setLoiteringDelay(500)
+      .build
+  }
+
+  def requestLocationUpdates {
+    val request = LocationRequest.create
+      .setInterval(5)
+      .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+    LocApi.requestLocationUpdates(apiClient, request, this)
+  }
 
   def locationConnected(data: Bundle)
 }
