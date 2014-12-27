332d299f4c1d923c426f7a492353f2f60de213a3
tek <torstenschmits@gmail.com>
2014-12-27 11:49:38 +0100

    skeleton for implementing themes programmatically

---
+++ src/res/style.scala
@@ -0,0 +1,69 @@
+package tryp.tick.res
+
+import scala.collection.mutable.{Map => MMap}
+
+import android.content.Context
+
+import tryp.droid.res.Resources
+
+abstract class Style
+{
+  Themes.add(this)
+
+  def name: String
+}
+
+object Style
+{
+  var resourcesInstance: Resources = null
+
+  def resources(implicit c: Context) = {
+    if (resourcesInstance == null) {
+      resourcesInstance = new Resources()
+    }
+    resourcesInstance
+  }
+
+  def apply(implicit c: Context) = {
+    Themes.get(resources.pref("theme"))
+  }
+}
+
+abstract class Default
+extends Style
+{
+  def button = {
+
+  }
+}
+
+object Default
+extends Default
+{
+  def name = "default"
+}
+
+class DarkGrin
+extends Default
+{
+  def name = "dark_grin"
+}
+
+class LightBlu
+extends Default
+{
+  def name = "light_blu"
+}
+
+object Themes
+{
+  val themes = MMap[String, Style]()
+
+  def add(theme: Style) {
+    themes(theme.name) = theme
+  }
+
+  def get(name: String) = {
+    themes.get(name) getOrElse Default
+  }
+}
