6681747f58d7139162b5e1019885f7fb9f46e98c
tek <torstenschmits@gmail.com>
2014-12-30 08:15:09 +0100

    remove isNone in favor of scalaz's implementation

--- src/util/core_ext/option.scala
+++ src/util/core_ext/option.scala
@@ -3,11 +3,4 @@ package tryp.droid.util
 trait OptionExt
 {
   import Control._
-
-  implicit class `tryp Option extensions`[A](o: Option[A]) {
-    def ifNone(f: ⇒ Unit) = {
-      o.isEmpty ? f
-      o
-    }
-  }
 }
