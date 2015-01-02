8c61d5db195be546183135a491cac7bc8f5513f2
tek <torstenschmits@gmail.com>
2015-01-02 15:47:16 +0100

    change asyncTask callback signature

--- src/basic.scala
+++ src/basic.scala
@@ -19,9 +19,9 @@ extends HasContext
   def theme(implicit ns: ResourceNamespace = GlobalResourceNamespace) =
     res.theme
 
-  def asyncTask[A, B](task: () ⇒ B)(callback: (B) ⇒ Unit) = {
+  def asyncTask[A, B](task: ⇒ B)(callback: (B) ⇒ Unit) = {
     new android.os.AsyncTask[A, Unit, B] {
-      override def doInBackground(args: A*) = task()
+      override def doInBackground(args: A*) = task
       override def onPostExecute(result: B) { callback(result) }
     }.execute()
   }
