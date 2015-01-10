73f5e46c2271724ee681e70747a02177644deded
tek <torstenschmits@gmail.com>
2015-01-10 14:17:32 +0100

    assertion combinators for options in unit

--- unit/src/app_test.scala
+++ unit/src/app_test.scala
@@ -2,15 +2,36 @@ package tryp.droid.test
 
 import org.scalatest._
 
-class AppTest
+class TrypTest
 extends FeatureSpec
 with RobolectricSuite
 with Matchers
-with BeforeAndAfter
+with BeforeAndAfterEach
 with BeforeAndAfterAll
 with LoneElement
+with TrypTestExt
 {
   override protected def beforeAll() {
     tryp.droid.Env.test = true
+  }
+}
+
+trait TrypTestExt
+extends Matchers
+{
+  implicit class `Option with assertion`[A](o: Option[A]) {
+    o should be('defined)
+
+    def foreachA(f: A ⇒ Unit) {
+      o foreach f
+    }
+
+    def flatMapA[B](f: A ⇒ Option[B]) = {
+      o flatMap f
+    }
+
+    def mapA[B](f: A ⇒ B) = {
+      o map f
+    }
   }
 }
