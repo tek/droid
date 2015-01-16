de7508da8681f78c1bbe32bed09a006dfe1b3f03
tek <torstenschmits@gmail.com>
2015-01-16 11:40:34 +0100

    move transition classes into object Transitions

--- src/view/transition.scala
+++ src/view/transition.scala
@@ -6,33 +6,36 @@ import android.transitions.everywhere._
 
 import tryp.droid.Macroid._
 
-case class TrypTransition[A <: View](name: String, trans: Transition,
-  view: Ui[A])
-{
-  trans.addTarget(name)
-
-  def get = view <~ transitionName(name)
-}
-
-abstract class TrypTransitions
-extends ActivityContexts
-{
-  def go(root: ViewGroup, view: View) {
-    val s = new Scene(root, view)
-    val set = new TransitionSet()
-    transitions foreach { t ⇒ set.addTransition(t) }
-    set.setOrdering(ordering)
-    set.setDuration(duration)
-    TransitionManager.go(s, set)
-  }
-
-  val transitions: Seq[Transition]
-  val ordering = TransitionSet.ORDERING_TOGETHER
-  val duration = 400
-}
-
 object Transitions
 {
+
+  case class TrypTransition[A <: View](name: String, trans: Transition,
+    view: Ui[A])
+  {
+    trans.addTarget(name)
+
+    def get = view <~ tweak
+
+    val tweak = transitionName(name)
+  }
+
+  abstract class TrypTransitions
+  extends ActivityContexts
+  {
+    def go(root: ViewGroup, view: View) {
+      val s = new Scene(root, view)
+      val set = new TransitionSet()
+      transitions foreach { t ⇒ set.addTransition(t) }
+      set.setOrdering(ordering)
+      set.setDuration(duration)
+      TransitionManager.go(s, set)
+    }
+
+    val transitions: Seq[Transition]
+    val ordering = TransitionSet.ORDERING_TOGETHER
+    val duration = 400
+  }
+
   implicit class `Slot transition helper`[A <: ViewGroup](root: Slot[A])
   {
     def transitionTo(trans: TrypTransitions, view: Ui[View]) {
