0066a5296decaa631897ec19fb8e51acf76caa7b
tek <torstenschmits@gmail.com>
2015-01-22 11:51:52 +0100

    allow belatedly adding navigation items

--- src/navigation.scala
+++ src/navigation.scala
@@ -3,11 +3,23 @@ package tryp.droid
 case class NavigationTarget(title: String, fragment: () ⇒ Fragment,
   home: Boolean = false)
 
-case class Navigation(targets: NavigationTarget*)
+class Navigation(initial: NavigationTarget*)
 {
+  val targets: Buffer[NavigationTarget] = initial.toBuffer
+
   def drawerItems = targets
 
   var current: Option[NavigationTarget] = None
 
   def isCurrent(target: NavigationTarget) = current.contains(target)
+
+  def ++=(addition: Seq[NavigationTarget]) = {
+    targets ++= addition
+    this
+  }
+}
+
+object Navigation
+{
+  def apply(initial: NavigationTarget*) = new Navigation(initial: _*)
 }
