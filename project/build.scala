d1ea06f7a94c6c512a5d22e77de6c4ca2b7b94ef
tek <torstenschmits@gmail.com>
2015-01-24 20:25:25 +0100

    move dependencies to extra file

--- project/build.scala
+++ project/build.scala
@@ -2,45 +2,6 @@ import sbt._
 import sbt.Keys._
 import android.Keys._
 
-object DroidDeps
-extends tryp.Deps
-{
-  override def deps = super.deps ++ Map(
-    "root" → common,
-    "slickmacros" → slickmacros
-  )
-
-  lazy val common = Seq(
-    resolvers += "jcenter" at "http://jcenter.bintray.com",
-    libraryDependencies ++= Seq(
-      aar("com.android.support" % "appcompat-v7" % "21.+"),
-      aar("com.android.support" % "palette-v7" % "21.+"),
-      aar("com.android.support" % "recyclerview-v7" % "21.+"),
-      aar("com.android.support" % "cardview-v7" % "21.+"),
-      "com.google.android.gms" % "play-services-maps" % "6.+",
-      "com.google.android.gms" % "play-services-location" % "6.+",
-      aar("org.macroid" %% "macroid" % "2.0.0-SNAPSHOT"),
-      "com.typesafe.akka" %% "akka-actor" % "2.+",
-      "org.scalaz" %% "scalaz-core" % "7.+",
-      "com.scalarx" %% "scalarx" % "0.+",
-      "com.github.andkulikov" % "transitions-everywhere" % "1.+",
-      "com.melnykov" % "floatingactionbutton" % "1.+",
-      "com.android.support" % "support-v13" % "21.+"
-    )
-  )
-
-  lazy val slickmacros = Seq(
-    libraryDependencies ++= Seq(
-      "com.typesafe.slick" %% "slick" % "2.+",
-      "org.slf4j" % "slf4j-nop" % "1.+",
-      "joda-time" % "joda-time" % "2.+",
-      "org.joda" % "joda-convert" % "1.+",
-      "org.scala-lang" % "scala-reflect" % "2.11.4",
-      "org.scala-lang" % "scala-compiler" % "2.11.4"
-    )
-  )
-}
-
 object DroidProguard
 extends tryp.Proguard
 {
