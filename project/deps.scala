1e50a3894ea0e441c3fa090022496c8c1aac9f68
tek <torstenschmits@gmail.com>
2015-05-24 16:53:18 +0200

    be more specific about versions of akka and specs2

--- project/deps.scala
+++ project/deps.scala
@@ -12,8 +12,12 @@ extends tryp.AndroidDeps
   )
 
   lazy val common = Seq(
-    resolvers += "jcenter" at "http://jcenter.bintray.com",
+    resolvers ++= Seq(
+      "Scalaz Bintray Repo" at "https://dl.bintray.com/scalaz/releases",
+      "jcenter" at "http://jcenter.bintray.com"
+    ),
     libraryDependencies ++= Seq(
+      "org.scalaz" %% "scalaz-concurrent" % "7.1.+",
       aar("com.android.support" % "appcompat-v7" % "21.+"),
       aar("com.android.support" % "palette-v7" % "21.+"),
       aar("com.android.support" % "recyclerview-v7" % "21.+"),
@@ -22,7 +26,7 @@ extends tryp.AndroidDeps
       "com.google.android.gms" % "play-services-location" % "6.+",
       "com.google.android.gms" % "play-services-plus" % "6.+",
       aar("org.macroid" %% "macroid" % "2.0.0-SNAPSHOT"),
-      "com.typesafe.akka" %% "akka-actor" % "2.+",
+      "com.typesafe.akka" %% "akka-actor" % "2.3.+",
       "com.scalarx" %% "scalarx" % "0.+",
       "com.github.andkulikov" % "transitions-everywhere" % "1.+",
       "com.melnykov" % "floatingactionbutton" % "1.+",
@@ -33,23 +37,19 @@ extends tryp.AndroidDeps
   )
 
   lazy val slickCore = Seq(
-    resolvers ++= Seq(
-      "Scalaz Bintray Repo" at "https://dl.bintray.com/scalaz/releases"
-      ),
     libraryDependencies ++= Seq(
       "com.typesafe.slick" %% "slick" % "2.+",
       "org.slf4j" % "slf4j-nop" % "1.+",
       "org.scala-lang" % "scala-reflect" % scalaVersion.value,
       "org.scala-lang" % "scala-compiler" % scalaVersion.value,
-      "org.scalaz" %% "scalaz-core" % "7.1.+",
-      "org.specs2" %%  "specs2-core" % "3.+" % "test"
+      "org.specs2" %%  "specs2-core" % "3.6" % "test"
     )
   )
 
   lazy val slick = Seq(
     libraryDependencies ++= Seq(
       "io.argonaut" %% "argonaut" % "6.+",
-      "org.specs2" %%  "specs2-core" % "3.+" % "test",
+      "org.specs2" %%  "specs2-core" % "3.6" % "test",
       "org.xerial" % "sqlite-jdbc" % "3.+" % "test"
     )
   )
