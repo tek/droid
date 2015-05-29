83eef7b86c97e1f02ebf5a1e7f8d7cb0a8ea24d3
tek <torstenschmits@gmail.com>
2015-05-29 10:54:28 +0200

    add idList method to model seqs

--- slick-core/src/model.scala
+++ slick-core/src/model.scala
@@ -16,20 +16,14 @@ trait HasObjectId
 
 object HasObjectId
 {
-  implicit class `seq of HasObjectId`(seq: Seq[_ <: HasObjectId]) {
-    def ids = seq map(_.id)
+  implicit class `seq of HasObjectId`(seq: Traversable[_ <: HasObjectId]) {
+    def idList = seq map(_.id) toList
+    def ids = idList.toSet
   }
 }
 
 trait Model
 extends HasObjectId
-
-object Model
-{
-  implicit class `DB Model sequence`[A <: Model](seq: Seq[A]) {
-    def ids = seq.map(_.id).toSet
-  }
-}
 
 trait Timestamps[A]
 {
