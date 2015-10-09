5042a8f0af3277db06a613c12e81bddf92188eab
tek <torstenschmits@gmail.com>
2015-10-09 19:45:41 +0200

    update build to current tryp-android

--- project/build.scala
+++ project/build.scala
@@ -5,30 +5,23 @@ import android.Keys._
 object DroidBuild
 extends tryp.AndroidBuild(deps = DroidDeps)
 {
-  override def globalSettings = super.globalSettings ++ Seq(
+  override def adefaults = super.adefaults ++ Seq(
     typedResources in Android := false,
-    lintEnabled in Android := false,
-    organization := "tryp",
-    scalacOptions += "-target:jvm-1.7"
+    lintEnabled in Android := false
   )
 
   override val prefix = Some("droid")
 
-  def ddp(name: String) = tdp(name).aar
-
-  lazy val core = ddp("core")
+  lazy val core = aar("core")
     .settingsV(description := "Common tryp stuff")
-    .transitive()
+    .transitive
+    .dep()
 
-  lazy val test = ddp("test")
-    .dep(core)
+  lazy val test = aar("test") << core
 
-  lazy val unit = ddp("unit")
-    .dep(test)
+  lazy val unit = aar("unit") << test
 
-  lazy val debug = ddp("debug")
-    .dep(core)
+  lazy val debug = aar("debug") << core
 
-  lazy val integration = ddp("integration")
-    .dep(test)
+  lazy val integration = aar("integration") << test
 }
