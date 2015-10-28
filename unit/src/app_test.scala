da11c047e8e6f910f32dd7fc1ef6a92bbcf69ba5
tek <torstenschmits@gmail.com>
2015-10-28 12:55:56 +0100

    move scaltest matchers to scalatest trait

--- unit/src/app_test.scala
+++ unit/src/app_test.scala
@@ -2,15 +2,10 @@ package tryp
 package droid
 package test
 
-import reflect.classTag
-
 import android.support.v7.widget.RecyclerView
 
 import org.robolectric.Robolectric
 import org.robolectric.shadows.ShadowApplication
-
-import org.scalatest._
-import matchers._
 
 trait TrypUnitSpec[A <: Activity with TrypTestActivity]
 extends TrypSpecExt
@@ -59,19 +54,4 @@ extends TrivialImplTestHelpers
       recycler
     }
   }
-
-  class TrypOptionMatcher[A: ClassTag]
-  extends Matcher[Option[A]]
-  {
-    def apply(o: Option[A]) = {
-      val tp = classTag[A].className
-      MatchResult(
-        o.isDefined,
-        s"Element of type $tp not present",
-        s"Element of type $tp present"
-      )
-    }
-  }
-
-  def bePresent[A: ClassTag] = new TrypOptionMatcher[A]
 }
--- unit/src/scalatest.scala
+++ unit/src/scalatest.scala
@@ -2,6 +2,8 @@ package tryp
 package droid
 package test
 
+import reflect.classTag
+
 import org.scalatest._
 import matchers._
 
@@ -14,3 +16,19 @@ with BeforeAndAfterEach
 with BeforeAndAfterAll
 with LoneElement
 with concurrent.ScalaFutures
+{
+  class TrypOptionMatcher[A: ClassTag]
+  extends Matcher[Option[A]]
+  {
+    def apply(o: Option[A]) = {
+      val tp = classTag[A].className
+      MatchResult(
+        o.isDefined,
+        s"Element of type $tp not present",
+        s"Element of type $tp present"
+      )
+    }
+  }
+
+  def bePresent[A: ClassTag] = new TrypOptionMatcher[A]
+}
