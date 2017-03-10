5f3e80ac9ce2baadafc674cfe358bfd228006cf2
Torsten Schmits <torstenschmits@gmail.com>
2017-03-10 21:56:10 +0100

    move legacy code out of the repo

--- .legacy/integration/src/base.scala
+++
@@ -1,9 +0,0 @@
-package tryp
-package droid
-package integration
-
-class IntStateActivity
-extends state.StateActivity
-{
-  override protected def mainViewTimeout = 30 seconds
-}
--- .legacy/integration/src/package.scala
+++
@@ -1,6 +0,0 @@
-package tryp
-package droid
-package integration
-
-@integrate(droid.state)
-object `package`
